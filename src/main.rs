b129a19fe4e357090d602d537a8bace790bc73fb
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-04-27 23:12:35 -0600

    feat: init project
    Can play a somewhat functional version of hangman

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use std::io;
+
+fn main() {
+    println!("Welcome to hangman!");
+
+    println!("Would you like to play? [yes/no]");
+
+    let mut lets_play = String::new();
+
+    io::stdin()
+        .read_line(&mut lets_play)
+        .expect("Not a string");
+
+    if lets_play == "no" {
+        good_bye();
+    } else if lets_play == "yes" {
+        println!("Let's go!");
+    }
+
+    let word = "Wunderbar";
+
+    let mut guessed_letters = String::new();
+
+    println!("Your word is this {} letters long!", word.len());
+
+    for _ in 0..word.len() {
+        print!("_ ");
+    }
+
+    loop {
+        let mut guessed_letter = String::new();
+
+        println!("Try a letter");
+
+        io::stdin()
+            .read_line(&mut guessed_letter)
+            .expect("to be a string");
+
+        if letter_matches(&guessed_letter, &word) {
+            println!("nice!");
+        } else {
+            println!("oops!");
+        }
+
+        guessed_letters.push_str(&guessed_letter.trim());
+    }
+
+    
+
+
+}
+
+fn good_bye() {
+    println!("Good bye!");
+    std::process::exit(0);
+}
+
+fn letter_matches(letter: &String, word: &str) -> bool {
+    let bytes = word.as_bytes();
+
+    for (_, &item) in bytes.iter().enumerate() {
+        if item == letter.as_bytes()[0] {
+            return true;
+        }
+    }
+
+    false
+}
